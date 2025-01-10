e730718da63afa7451e2c8a815428b9578354c76
zht298 <zht19886@gmail.com>
2025-01-10 17:54:39 +0800

    Update filter_playlist.py

--- filter_playlist.py
+++ filter_playlist.py
@@ -1,13 +1,8 @@
 import requests
-import re
 
 def fetch_playlist(url):
     response = requests.get(url)
     return response.text
-
-def clean_group_name(group_name):
-    # 去掉所有非中文、英文、数字和下划线的字符
-    return re.sub(r'[^\w\u4e00-\u9fa5]', '', group_name)
 
 def filter_groups(playlist, target_groups):
     filtered_lines = []
@@ -16,7 +11,7 @@ def filter_groups(playlist, target_groups):
 
     for line in lines:
         if '#genre#' in line:
-            current_group = clean_group_name(line.split(',')[0].strip())
+            current_group = line.split(',')[0].strip()
         if current_group in target_groups:
             filtered_lines.append(line)
 

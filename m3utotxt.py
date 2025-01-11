4aa3765dfc899da79577a506cbbd34b812ad5723
zht298 <zht19886@gmail.com>
2025-01-11 18:13:33 +0800

    Update m3utotxt.py

--- m3utotxt.py
+++ m3utotxt.py
@@ -1,12 +1,12 @@
 import requests
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs, urlunparse
 
 def download_m3u_file(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.text
 
-def parse_m3u_content(m3u_content, default_group_name):
+def parse_m3u_content(m3u_content, default_group_name, rename_channels=None, rename_groups=None):
     lines = m3u_content.splitlines()
     playlist = []
     current_group = default_group_name
@@ -24,6 +24,15 @@ def parse_m3u_content(m3u_content, default_group_name):
                     parts = line.split(',', 1)
                     channel_name = parts[1]
                     group_name = current_group
+                
+                # Rename groups if specified
+                if rename_groups and group_name in rename_groups:
+                    group_name = rename_groups[group_name]
+                
+                # Rename channels if specified
+                if rename_channels and channel_name in rename_channels:
+                    channel_name = rename_channels[channel_name]
+
             except IndexError:
                 print(f"Skipping malformed line: {line}")
                 continue
@@ -43,23 +52,42 @@ def save_playlist_to_txt(playlist, txt_filename):
             file.write(f"{channel_name},{link}\n")
 
 def process_m3u_urls(m3u_urls):
-    for url in m3u_urls:
+    for url_info in m3u_urls:
+        url = url_info['url']
         parsed_url = urlparse(url)
         query_params = parse_qs(parsed_url.query)
-        default_group_name = query_params.get('group', ['default'])[0]
+        default_group_name = query_params.get('group', [url_info.get('default_group', 'default')])[0]
+        
+        # If no group is specified in the URL, add the default group
+        if 'group' not in query_params:
+            query_params['group'] = [url_info.get('default_group', 'default')]
+            parsed_url = parsed_url._replace(query=urlunparse(query_params))
+            url = urlunparse(parsed_url)
 
         # 下载 M3U 文件内容（忽略 URL 中的查询参数）
         m3u_content = download_m3u_file(url.split('?')[0])
         m3u_filename = parsed_url.path.split("/")[-1]
         txt_filename = m3u_filename.replace('.m3u', '.txt')
         
-        playlist = parse_m3u_content(m3u_content, default_group_name)
+        playlist = parse_m3u_content(
+            m3u_content, 
+            default_group_name, 
+            rename_channels=url_info.get('rename_channels'),
+            rename_groups=url_info.get('rename_groups')
+        )
         save_playlist_to_txt(playlist, txt_filename)
 
 def main():
     m3u_urls = [
-        "https://raw.githubusercontent.com/zht298/IPTVlist/main/playlist.m3u",
-        "http://adultiptv.net/chs.m3u?group=直播",  # 没有 group 参数
+        {
+            "url": "https://raw.githubusercontent.com/zht298/IPTVlist/main/playlist.m3u",
+            "rename_channels": {"央视频道": "央视"},
+        },
+        {
+            "url": "http://adultiptv.net/chs.m3u",
+            "default_group": "直播",
+            "rename_groups": {"xxx": "点播"},
+        },
         # 添加更多的链接，并在URL中指定自定义频道分组名称
     ]
     process_m3u_urls(m3u_urls)
